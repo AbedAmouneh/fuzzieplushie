e157d3dd3dacd9e54bf01dc4db7d0532ec451584
TheeBrainiacs <145912020+Kronbii@users.noreply.github.com>
2024-01-20 00:44:23 +0200

    navbar edit
    -change sections width

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './Navbar.css';
 import logo from "./assets/Logo.svg";
+import shopCart from "./assets/shopCart.svg"
 
 function Navbar() {
 
@@ -12,11 +13,17 @@ function Navbar() {
                   <div className="navbar-text">Shop</div>
                   <div className="navbar-text">About us</div>
               </div>
-              <img src={logo} className="navbar-logo" alt="logo" />
+              <div className="navbar-logo-container">
+                  <img src={logo} className="navbar-logo" alt="logo" />
+                </div>
               <div className="right-navbar-section">
                   <div className="navbar-text">Customize</div>
                   <div className="navbar-text">Sign Up</div>
                   <div className="navbar-text">Log In</div>
+                    <div className="nav-search-container">
+                        <input className="nav-search" type="text" placeholder="search..." />
+                        <img src={shopCart} className="nav-shop" alt="shopcart" />
+                </div>
               </div>
           </div>
     </>
